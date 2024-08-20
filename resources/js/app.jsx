374394e37e903bb3106b9887f49876a860204fd8
AlexeyRudkovskiy <alexey.r@a-team.global>
2024-08-20 21:20:22 +0300

    Adds React to the project

---
+++ resources/js/app.jsx
@@ -0,0 +1,12 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import axios from "axios";
+import Application from "./Application.jsx";
+import './bootstrap';
+
+await axios.get('/sanctum/csrf-cookie')
+
+const root = document.getElementById('app');
+if (root !== null) {
+    createRoot(root).render(<Application></Application>)
+}
